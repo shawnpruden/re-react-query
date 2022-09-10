1a95ce4e62a7fd267f8d78291f17e4b1a860cb11
Shawn Pruden <dev.shawn@yahoo.com>
2022-09-09 23:23:53 -0700

    fetching data with useQuery

--- src/pages/RQSuperheroes.jsx
+++ src/pages/RQSuperheroes.jsx
@@ -1,5 +1,28 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchData = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
 
 export default function RQSuperheroes() {
-  return <div>RQSuperheroes</div>;
+  const { isLoading, data } = useQuery('super-heros', fetchData);
+
+  console.log(isLoading, data);
+
+  return (
+    <>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h2>React Query Superheroes</h2>
+          {data?.data.map(({ name }, index) => (
+            <p key={index}>{name}</p>
+          ))}
+        </>
+      )}
+    </>
+  );
 }
