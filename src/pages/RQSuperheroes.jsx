ca0d57fa6b97a51851ab5cd9dbf65570030bc9d4
Shawn Pruden <dev.shawn@yahoo.com>
2022-09-11 16:08:55 -0700

    custom query hook

--- src/pages/RQSuperheroes.jsx
+++ src/pages/RQSuperheroes.jsx
@@ -1,9 +1,4 @@
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
-
-const fetchData = () => {
-  return axios.get('http://localhost:4000/superheroes');
-};
+import { useData } from '../hooks';
 
 export default function RQSuperheroes() {
   const onSuccess = ({ data }) => {
@@ -14,31 +9,9 @@ export default function RQSuperheroes() {
     console.log('perform side effect after encountering error', err);
   };
 
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    ['super-heros'],
-    fetchData,
-    {
-      /*
-      cacheTime: 5000, // > default is 5 mins, data will be cashed for 5s (with background refetching) after user left current page; isLoading remains false
-
-      staleTime: 30000, // > default is 0, from fresh to stale after 30s (without background refetching within 30s / no additional requests); isLoading and isFetching both remain false
-      */
-      /*
-      refetchOnMount: 'always', // > default is true, no matter the query data is stale or not, the query will always refetch the data when component mounts
-
-      refetchOnWindowFocus: true, // > default is true, anytime the window loses focus and gain focus again a background refetching is initiated
-
-      refetchInterval: 2000, // > default is false, refetch data every 2s; pauses if window loses its focus
-      refetchIntervalInBackground: true, // > default is false, continue to refetch data even when window is not in focus
-      */
-      /* 
-      enabled: false, // > disabled fetching on mount
-      */
-
-      onSuccess,
-      onError, // > retry 3 times before calling this func
-      select: ({ data }) => data.map((hero) => hero.name), // > data transformation; return data contains an array of name
-    }
+  const { data, isLoading, isFetching, isError, error, refetch } = useData(
+    onSuccess,
+    onError
   );
 
   // > if data remains the same as cashed data, isLoading is not changed but there is a background refetching to ensure the data is up to date
