9f645bde7f86ab9538b33b8025b925c7c175a8ba
Shawn Pruden <dev.shawn@yahoo.com>
2022-09-10 22:07:56 -0700

    stale time

--- src/pages/RQSuperheroes.jsx
+++ src/pages/RQSuperheroes.jsx
@@ -10,7 +10,11 @@ export default function RQSuperheroes() {
   const { isLoading, data, isError, error, isFetching } = useQuery(
     ['super-heros'],
     fetchData,
-    { cacheTime: 5000 } // > default is 5 mins
+    {
+      cacheTime: 5000, // > default is 5 mins, data will be cashed for 5s (with background refetching) after user left current page, isLoading remains false
+
+      staleTime: 30000, // > default is 0, from fresh to stale after 30s (without background refetching within 30s / no additional requests), isLoading and isFetching both remain false
+    }
   );
 
   // > if data remains the same as cashed data, isLoading is not changed but there is a background refetching to ensure the data is up to date
