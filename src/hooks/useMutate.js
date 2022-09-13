569906a8acce6f56a4e9a8eb77fbe01067e3d1c9
Shawn Pruden <dev.shawn@yahoo.com>
2022-09-13 12:29:55 -0700

    handle mutation response

--- src/hooks/useMutate.js
+++ src/hooks/useMutate.js
@@ -6,9 +6,18 @@ const addData = (hero) => axios.post('http://localhost:4000/superheroes', hero);
 export default function useMutate() {
   const queryClient = useQueryClient();
 
+  // > addData automatically receives argument from mutate func where it's been used
   return useMutation(addData, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(['superheroes']); // > refetch query when new data has been added
+    onSuccess: (data) => {
+      /* 
+      queryClient.invalidateQueries(['superheroes']); // > refetch query to update ui when new data has been added
+      */
+
+      // > to save additional network request, update existing query data with new data instead of refetching query to update ui afterwords
+      queryClient.setQueryData(['superheroes'], (oldData) => ({
+        ...oldData,
+        data: [...oldData.data, data.data], // > add new hero object from mutation response to the array of superheroes
+      }));
     },
-  }); // > addData automatically receives argument from mutate func where it's been used
+  });
 }
