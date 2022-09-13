063dbfc591bd51b8f987c0f535ba7cf9603e95dd
Shawn Pruden <dev.shawn@yahoo.com>
2022-09-13 11:34:19 -0700

    refetch on mutation

--- src/hooks/useMutate.js
+++ src/hooks/useMutate.js
@@ -1,8 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 const addData = (hero) => axios.post('http://localhost:4000/superheroes', hero);
 
 export default function useMutate() {
-  return useMutation(addData); // > addData automatically receives argument from mutate func where it's been used
+  const queryClient = useQueryClient();
+
+  return useMutation(addData, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['superheroes']); // > refetch query when new data has been added
+    },
+  }); // > addData automatically receives argument from mutate func where it's been used
 }
